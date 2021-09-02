81ca840114731e323dd40cd61f9d31152350118d
macnick <nick@macexperts.gr>
2021-09-02 12:05:23 +0300

    Update the baseURL to the new URL

--- src/services/axios-api.js
+++ src/services/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://evening-cove-51744.herokuapp.com',
+  baseURL: 'https://bookaduc.herokuapp.com',
 });
 
 export const setAuthorizationToken = token => {
